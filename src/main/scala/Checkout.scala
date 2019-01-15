30b675b2ef41946f99a2c1025c3606a13633973c
jabiz <daniel.jabore@hotmail.com>
2019-01-15 11:53:31 +0000

    inital upload

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -1,13 +1,45 @@
 import scala.collection.mutable.ArrayBuffer
-class Checkout {
+object Checkout extends App{
 
   var price = 0
-  var checkout = ArrayBuffer()
-
-  var user_in = readLine
-  if (user_in == "Apple" || user_in  == "Orang"){
-    checkout.append()
+  var orangeCount= 0
+  var appleCount = 0
+  var checkout = scala.collection.mutable.ArrayBuffer(toString)
+  println("please enter how meany fruits you want")
+  var user_in_num = readInt()
+  print("please enter your fruts with lowecse")
+  for (i <- 1 to user_in_num) {
+    var user_in = readLine
+    if (user_in == "apple" || user_in == "orange") {
+      checkout.append(user_in)
+    }
+    else {
+      println("not a frute or at least not one we sell We only sell apple and orange with lowercase")
+      println( s"you get one more go otherwise i will come over personaly and eat your $user_in personally")
+      user_in = readLine
+      if (user_in == "apple" || user_in == "orange") {
+        checkout.append(user_in)
+      }
+    }
   }
+
+  checkout.foreach(iteration => {
+    if (iteration == "apple")
+      {
+        appleCount+=1
+      }
+    if (iteration == "orange")
+    {
+      orangeCount+=1
+    }
+  })
+
+  price += (appleCount*60)+(orangeCount*25)
+
+  print(price)
+
+
+  //checkout.foreach(println)
 
 
 
