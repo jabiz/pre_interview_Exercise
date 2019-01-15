491c1a6a5a52f81581f656ef6f2e33a426013892
jabiz <daniel.jabore@hotmail.com>
2019-01-15 11:15:22 +0000

    inital upload

---
+++ src/main/scala/Checkout.scala
@@ -0,0 +1,18 @@
+import scala.collection.mutable.ArrayBuffer
+class Checkout {
+
+  var price = 0
+  var checkout = ArrayBuffer()
+
+  var user_in = readLine
+  if (user_in == "Apple" || user_in  == "Orang"){
+    checkout.append()
+  }
+
+
+
+
+
+
+
+}
